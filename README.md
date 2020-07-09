c08f3f01face45c202798f74edb9aca0b0bb858f
zderApfel <scoutt172@yahoo.com>
2020-07-09 17:07:11 -0400

    Version 1.0 push

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-My first discord bot, created as a personal introduction to Discordjs
+<h1>Sayu, the bot that speaks for you</h1>
+
+Hello there! This is Sayu, my first ever Discord bot utilizing the Discordjs library. It's a very simple bot that takes a private message from a user and relays it into a channel. The user and the channel IDs are determined in the config.json file in this repo. 
+
+<h2>Version History</h2>
+
+<h3>1.0</h3>
+
+* Accepts a message from a user defined in `config.json` and delivers it to a specified channel in the server it is invited to.
+
+<h2>Possible future features</h2>
+
+* Allow to do mentions
+* Allow to be posted in different channels, given an argument
+* Emoji support
+* Image/Gif support
--- bot.js
+++ bot.js
@@ -1,16 +1,29 @@
-
-const Discord = require("discord.js");
-const config = require("./config/config.json");
+const Discord = require("discord.js"); //Initialization
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("I am ready!");
-});
+const config = require("./config/config.json"); //Configuration file for hidden parameters
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+//Parameters
+const botToken = config.token; //Bot token
+const userID = config.userID; //Person bot will be listening to messages from
+
+
+client.on("ready", () => {
+  console.log("Bot has logged in");
+  if(client.users.get(userID) != undefined){ //Prevent crash due to invalid UserID
+    client.users.get(userID).send("I AWAKEN. Say what you want me to say. NOTE: I currently cannot send emojis, but hopefully I can in the future :)");
+  }
+  else{
+    console.log("ERROR: User not found! Verify ID and try again");
   }
 });
 
-client.login(config.token); //Edit token in config.json
+client.on("message", (message) => {
+  let sendTo = client.channels.get(config.channelID);
+  if(message.author.id == userID && message.channel instanceof Discord.DMChannel){
+    sendTo.send(message.content);
+  }
+})
+
+client.login(botToken); //Edit token in config.json
+
