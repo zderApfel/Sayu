dc2e0dfcb14ecc5accbfec740115d39512b7b0c3
zderApfel <scoutt172@yahoo.com>
2020-08-05 18:58:35 -0400

    Dynamic channel command syntax made

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const config = require("./config/config.json"); //Configuration file for hidden 
 //Parameters
 const botToken = config.TOKEN; //Bot token
 const userID = config.USERID; //Person bot will be listening to messages from 
-const channels = config.CHANNELS;
+const chanList = config.CHANNELS;
 
 client.on("ready", () => {
   console.log("Bot has logged in");
   if(client.users.cache.get(userID) != undefined){ //Prevent crash due to invalid userID
-    client.users.cache.get(userID).send("I AWAKEN. Say what you want me to say. NOTE: I currently cannot send emojis, but hopefully I can in the future :)");
+    client.users.cache.get(userID).send("I AWAKEN. I can now send messages to multiple channels! Type !channels to see the channels I can send messages to!")
   }
   else{
     console.log("ERROR: User not found! Verify ID and try again");
@@ -20,13 +20,20 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  client.channels.cache.get(config.CHANNELS)
+  
+  //client.channels.cache.get()
   if(message.author.id == userID && message.channel instanceof Discord.DMChannel){
-    if (message.content.includes("!help") == true){
+    if (message.content.startsWith("!help") == true){
       client.users.cache.get(userID).send("This will be a help message");
     }
     else {
-      //sendTo.send(message.content);
+      client.users.cache.get(userID).send("Sorry, I don't understand you, type !help for the list of commands I can understand");
+    }
+    for (x in chanList){
+      if (message.content.startsWith(`!${chanList[x].name}`) == true){ //But this one doesn't
+        let sendTo = client.channels.cache.get(chanList[x].id); //Neither does this one
+        sendTo.send(message.content);
+      }
     }
   }
 })
