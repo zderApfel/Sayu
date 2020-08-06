61f34f93f3cd3b6329a56b4391402c7cdd3aa31b
zderApfel <scoutt172@yahoo.com>
2020-08-06 00:25:52 -0400

    Dynamic channel commands now function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const chanList = config.CHANNELS;
 client.on("ready", () => {
   console.log("Bot has logged in");
   if(client.users.cache.get(userID) != undefined){ //Prevent crash due to invalid userID
-    client.users.cache.get(userID).send("I AWAKEN. I can now send messages to multiple channels! Type !channels to see the channels I can send messages to!")
+    client.users.cache.get(userID).send("I AWAKEN. I can now send messages to multiple channels! Type !help for info!");
   }
   else{
     console.log("ERROR: User not found! Verify ID and try again");
@@ -21,26 +21,33 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if(message.author.id == userID && message.channel instanceof Discord.DMChannel){
-    let commandSuccess = false;
-    if (message.content.startsWith("!") == true){
-      if (message.content.startsWith("!help") == true){
-        commandSuccess = true;  
-        client.users.cache.get(userID).send("This will be a help message");
-      }
-      else{
-        for (x in chanList){
-          if (message.content.startsWith(`!${chanList[x].name}`) == true){
-            client.channels.cache.get(chanList[x].id).send(message.content);
-            commandSuccess = true;
-          }
-        }
-      }
+    let command = "";
+    if(message.content.startsWith("!") == true){
+      command = getCommand(message.content);
+      setCommand(command, message.content);
     }
-    if (commandSuccess == false){
-      client.users.cache.get(userID).send("Sorry, I don't understand you, type !help for the list of commands I can understand");
-    }
+      //client.users.cache.get(userID).send("This will be a help message");
+      //client.users.cache.get(userID).send("Sorry, I don't understand you, type !help for the list of commands I can understand");
+      //client.channels.cache.get(`channel id`).send("");
   }
 })
+
+function getCommand(text){
+  let newText = text.split(" ");
+  return newText[0];
+}
+
+function setCommand(comm, fullMessage){
+  if (comm == "!help"){
+    return client.users.cache.get(userID).send("This will be a help message");
+  }
+  for (x in chanList){
+    if (comm == `!${chanList[x].name.toLowerCase()}`){
+      return client.channels.cache.get(chanList[x].id).send(fullMessage.slice(comm.length + 1));
+    }
+  }
+  client.users.cache.get(userID).send("Sorry, I don't understand you, type !help for the list of commands I can understand");
+}
 
 client.login(botToken); //Edit token in config.json
 
